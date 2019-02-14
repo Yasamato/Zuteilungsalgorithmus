8a433196c391830c249e521fb520006901ae43ae
Agent77326 <agent77326@gmail.com>
2019-02-14 23:05:20 +0100

    Integration von TCPDF-6.2.26
    Die Lib: https://tcpdf.org/ wurde eingebunden und integriert in den bestehenden code, sodass unabhängig vom Browser bei allen jetzt die gleiche PDF serverseitig via php erstellt wird.
    
    Abgesehen davon hab ich die Bilder in den Ordner pictures verschoben und die Referenzen dazu geändert.

--- web/index.php
+++ web/index.php
@@ -45,104 +45,7 @@
 	session_start();
   (include "../data/config.php") OR die("</head><body style='color: #000'>Der Webserver wurde noch nicht konfiguriert, kontaktiere einen Admin damit dieser setup.sh ausführt.</body></html>");
 	require "php/db.php";
-
-	if (!file_exists("../data/config.csv")) {
-		// define the header of the columns in the first row
-		dbCreateFile("../data/config.csv", [
-			"Stage",
-			"Schüleranzahl",
-			"MontagVormittag",
-			"MontagVormittagHinweis",
-			"MontagNachmittag",
-			"MontagNachmittagHinweis",
-			"DienstagVormittag",
-			"DienstagVormittagHinweis",
-			"DienstagNachmittag",
-			"DienstagNachmittagHinweis",
-			"MittwochVormittag",
-			"MittwochVormittagHinweis",
-			"MittwochNachmittag",
-			"MittwochNachmittagHinweis",
-			"DonnerstagVormittag",
-			"DonnerstagVormittagHinweis",
-			"DonnerstagNachmittag",
-			"DonnerstagNachmittagHinweis",
-			"FreitagVormittag",
-			"FreitagVormittagHinweis",
-			"FreitagNachmittag",
-			"FreitagNachmittagHinweis"
-		]);
-		dbAdd("../data/config.csv", [
-			0,
-			800,
-			"true",
-			"",
-			"true",
-			"",
-			"true",
-			"",
-			"true",
-			"",
-			"true",
-			"",
-			"true",
-			"",
-			"true",
-			"",
-			"true",
-			"",
-			"true",
-			"",
-			"false",
-			""
-		]);
-	}
-	$config = dbRead("../data/config.csv")[0];
-
-	if (!file_exists("../data/projekte.csv")) {
-		// define the header of the columns in the first row
-		dbCreateFile("../data/projekte.csv", [
-			"id",
-			"name",
-			"beschreibung",
-			"betreuer",
-			"minKlasse",
-			"maxKlasse",
-			"minPlatz",
-			"maxPlatz",
-			"vorraussetzungen",
-			"sonstiges",
-			"raum",
-			"material",
-			"moVor",
-			"moMensa",
-			"moNach",
-			"diVor",
-			"diMensa",
-			"diNach",
-			"miVor",
-			"miMensa",
-			"miNach",
-			"doVor",
-			"doMensa",
-			"doNach",
-			"frVor",
-			"frMensa",
-			"frNach"
-		]);
-	}
-
-	if (!file_exists("../data/wahl.csv")) {
-		// define the header of the columns in the first row
-		dbCreateFile("../data/wahl.csv", [
-			"uid",
-			"vorname",
-			"nachname",
-			"stufe",
-			"klasse",
-			"wahl"
-		]);
-	}
+	require "php/utils.php";
 ?>
 
 	<script>
@@ -170,41 +73,6 @@
 	</script>
 
 <?php
-	function isLogin() {
-		return isset($_SESSION['benutzer']);
-	}
-
-	function logout() {
-		session_destroy();
-		session_start();
-	}
-
-	function alert($msg) {
-		echo "<script>alert('" . $msg . "');</script>";
-	}
-
-	function newlineBack($txt) {
-	  $txt = str_replace("<br>", "\n", $txt);
-	  $txt = str_replace("<br/>", "\n", $txt);
-	  return str_replace("<br />", "\n", $txt);
-	}
-
-	function getProjektInfo($id) {
-	  foreach (dbRead("../data/projekte.csv") as $projekt) {
-	    if ($projekt["id"] == $id) {
-	      return $projekt;
-	    }
-	  }
-	}
-
-	function checkBox($v) {
-		return isset($_POST[$v]) && $_POST[$v] ? "true" : "false";
-	}
-
-	// htmlentities() ?
-	function newlineRemove($txt) {
-		return str_replace("\n", "<br>", $txt);
-	}
 
 	// on form-submit
 	if (isset($_GET['logout'])) {
@@ -428,7 +296,7 @@
 				</button>
 			</div>
 <?php } ?>
-			<img class="mb-4" src="wahlbox.svg" alt="Wahlbox" width="144" height="144">
+			<img class="mb-4" src="pictures/wahlbox.svg" alt="Wahlbox" width="144" height="144">
 			<h1 class="h3 mb-3 font-weight-normal">Anmeldung</h1>
 			<label for="inputBenutzername" class="sr-only">Benutzername</label>
 			<input type="text" name="user" id="inputBenutzername" class="form-control" placeholder="Benutzername" required autofocus>
@@ -444,8 +312,6 @@
 			<input type="hidden" name="action" value="logout">
 		</form>
 		<div class="tmp-modal"></div>
-		<div id="section-to-print"></div>
-
 
 		<footer class="footer">
 			<div class="container text-center">
---
+++ web/printPDF.php
@@ -0,0 +1,202 @@
+<?php
+	session_start();
+  (include "../data/config.php") OR die("</head><body style='color: #000'>Der Webserver wurde noch nicht konfiguriert, kontaktiere einen Admin damit dieser setup.sh ausführt.</body></html>");
+	require "php/db.php";
+	require 'php/utils.php';
+  require 'php/TCPDF-6.2.26/tcpdf.php';
+
+  if (!isLogin() || $_SESSION['benutzer']['typ'] != "admin" && $_SESSION['benutzer']['typ'] != "lehrer") {
+    die("Zugriff verweigert");
+  }
+
+  class printPDF extends TCPDF {
+    // Breite: 297mm - 2cm margin => 277mm zum Arbeiten
+    function printProjekt($projekt) {
+      $this->AddPage();
+      $this->setCellHeightRatio(1.1);
+      $this->ln(13);
+
+      // Zeile 1
+      $this->SetFont('freeserif', 'B', 14);
+      $this->Cell(180, 0, 'Projekttitel:', "LTR");
+      $this->Cell(97, 0, 'Betreuer:', "TR");
+      $this->SetFont('freeserif', '', 10);
+      $this->ln();
+      $this->Cell(180, 0, $projekt["name"], "LBR");
+      $this->Cell(97, 0, $projekt["betreuer"], "BR");
+      $this->ln(6);
+
+      // Zeile 2
+      $this->SetFont('freeserif', 'B', 14);
+      $this->Cell(22, 0, 'Stufen:', "LTR");
+      $this->Cell(52, 0, 'Teilnehmeranzahl:', "TR");
+      $this->Cell(203, 0, 'Kosten/Sonstiges:', "TR");
+      $this->SetFont('freeserif', '', 10);
+      $this->ln();
+      $this->Cell(22, 0, $projekt["minKlasse"] . " - " . $projekt["maxKlasse"], "LBR");
+      $this->Cell(52, 0, $projekt["minPlatz"] . " - " . $projekt["maxPlatz"], "BR");
+      $this->Cell(203, 0, $projekt["sonstiges"], "BR");
+      $this->ln(6);
+
+      // Zeile 3
+      $this->SetFont('freeserif', 'B', 14);
+      $this->Cell(277, 0, 'Vorraussetzungen:', "LTR");
+      $this->SetFont('freeserif', '', 10);
+      $this->ln();
+      $this->Cell(277, 0, $projekt["vorraussetzungen"], "LBR");
+      $this->ln(6);
+
+      // Zeile 4
+      $this->SetFont('freeserif', 'B', 14);
+      $this->Cell(0, 0, 'Beschreibung:', "LTR");
+      $this->SetFont('freeserif', '', 10);
+      $this->ln();
+      $y = $this->getY();
+      //$this->Write(5, newlineBack($projekt["beschreibung"]), '', 0, 'L', false, 0, false, false, 0);
+      //$this->MultiCell(0, 5, newlineBack($projekt["beschreibung"]), "LBR", "L");
+      $this->writeHTMLCell(0, 0, 10, $y, $projekt["beschreibung"]);
+      $this->Line(10, $y, 10, $y + 80);
+      $this->Line(10, $y + 80, 287, $y + 80);
+      $this->Line(287, $y, 287, $y + 80);
+      $this->ln(82);
+
+      // Zeile 5
+      $this->SetFont('freeserif', 'B', 14);
+      $this->Cell(277, 0, 'Vorraussichtlicher Ablauf', 0, 0, "C");
+      $this->ln(6);
+
+      // Tabelle
+      // Zeile 6
+      $this->SetFont('freeserif', 'B', 14);
+      $this->Cell(27, 0, 'Tag', "LTRB");
+      $this->Cell(50, 0, 'Montag', "TRB");
+      $this->Cell(50, 0, 'Dienstag', "TRB");
+      $this->Cell(50, 0, 'Mittwoch', "TRB");
+      $this->Cell(50, 0, 'Donnerstag', "TRB");
+      $this->Cell(50, 0, 'Freitag', "TRB");
+      $this->ln();
+
+      //Zeile 7 Vormittag
+      $y = $this->getY();
+      $h = 20;
+      $this->SetFont('freeserif', 'B', 10);
+      $this->Cell(27, 0, "Vormittag");
+      $this->Line(10, $y, 10, $y + $h);
+      $this->Line(10, $y + $h, 37, $y + $h);
+      $this->Line(37, $y, 37, $y + $h);
+      $this->SetFont('freeserif', '', 10);
+      $this->writeHTMLCell(50, 0, 37, $y, $projekt["moVor"]);
+      $this->Line(37, $y, 37, $y + $h);
+      $this->Line(37, $y + $h, 87, $y + $h);
+      $this->Line(87, $y, 87, $y + $h);
+      $this->writeHTMLCell(50, 0, 87, $y, $projekt["diVor"]);
+      $this->Line(87, $y, 87, $y + $h);
+      $this->Line(87, $y + $h, 137, $y + $h);
+      $this->Line(137, $y, 137, $y + $h);
+      $this->writeHTMLCell(50, 0, 137, $y, $projekt["miVor"]);
+      $this->Line(137, $y, 137, $y + $h);
+      $this->Line(137, $y + $h, 187, $y + $h);
+      $this->Line(187, $y, 187, $y + $h);
+      $this->writeHTMLCell(50, 0, 187, $y, $projekt["doVor"]);
+      $this->Line(187, $y, 187, $y + $h);
+      $this->Line(187, $y + $h, 237, $y + $h);
+      $this->Line(237, $y, 237, $y + $h);
+      $this->writeHTMLCell(50, 0, 237, $y, $projekt["frVor"]);
+      $this->Line(237, $y, 237, $y + $h);
+      $this->Line(237, $y + $h, 287, $y + $h);
+      $this->Line(287, $y, 287, $y + $h);
+      $this->ln($h);
+      //Zeile 7 Mensa
+      $y = $this->getY();
+      $h = 5;
+      $this->SetFont('freeserif', 'B', 10);
+      $this->Cell(27, 0, "Mensa");
+      $this->Line(10, $y, 10, $y + $h);
+      $this->Line(10, $y + $h, 37, $y + $h);
+      $this->Line(37, $y, 37, $y + $h);
+      $this->SetFont('freeserif', '', 10);
+      $this->writeHTMLCell(50, 0, 37, $y, $projekt["moMensa"] == "true" ? "Ja" : "Nein");
+      $this->Line(37, $y, 37, $y + $h);
+      $this->Line(37, $y + $h, 87, $y + $h);
+      $this->Line(87, $y, 87, $y + $h);
+      $this->writeHTMLCell(50, 0, 87, $y, $projekt["diMensa"] == "true" ? "Ja" : "Nein");
+      $this->Line(87, $y, 87, $y + $h);
+      $this->Line(87, $y + $h, 137, $y + $h);
+      $this->Line(137, $y, 137, $y + $h);
+      $this->writeHTMLCell(50, 0, 137, $y, $projekt["miMensa"] == "true" ? "Ja" : "Nein");
+      $this->Line(137, $y, 137, $y + $h);
+      $this->Line(137, $y + $h, 187, $y + $h);
+      $this->Line(187, $y, 187, $y + $h);
+      $this->writeHTMLCell(50, 0, 187, $y, $projekt["doMensa"] == "true" ? "Ja" : "Nein");
+      $this->Line(187, $y, 187, $y + $h);
+      $this->Line(187, $y + $h, 237, $y + $h);
+      $this->Line(237, $y, 237, $y + $h);
+      $this->writeHTMLCell(50, 0, 237, $y, $projekt["frMensa"] == "true" ? "Ja" : "Nein");
+      $this->Line(237, $y, 237, $y + $h);
+      $this->Line(237, $y + $h, 287, $y + $h);
+      $this->Line(287, $y, 287, $y + $h);
+      $this->ln($h);
+      //Zeile 7 Nachmittag
+      $y = $this->getY();
+      $h = 20;
+      $this->SetFont('freeserif', 'B', 10);
+      $this->Cell(27, 0, "Nachmittag");
+      $this->Line(10, $y, 10, $y + $h);
+      $this->Line(10, $y + $h, 37, $y + $h);
+      $this->Line(37, $y, 37, $y + $h);
+      $this->SetFont('freeserif', '', 10);
+      $this->writeHTMLCell(50, 0, 37, $y, $projekt["moNach"]);
+      $this->Line(37, $y, 37, $y + $h);
+      $this->Line(37, $y + $h, 87, $y + $h);
+      $this->Line(87, $y, 87, $y + $h);
+      $this->writeHTMLCell(50, 0, 87, $y, $projekt["diNach"]);
+      $this->Line(87, $y, 87, $y + $h);
+      $this->Line(87, $y + $h, 137, $y + $h);
+      $this->Line(137, $y, 137, $y + $h);
+      $this->writeHTMLCell(50, 0, 137, $y, $projekt["miNach"]);
+      $this->Line(137, $y, 137, $y + $h);
+      $this->Line(137, $y + $h, 187, $y + $h);
+      $this->Line(187, $y, 187, $y + $h);
+      $this->writeHTMLCell(50, 0, 187, $y, $projekt["doNach"]);
+      $this->Line(187, $y, 187, $y + $h);
+      $this->Line(187, $y + $h, 237, $y + $h);
+      $this->Line(237, $y, 237, $y + $h);
+      $this->writeHTMLCell(50, 0, 237, $y, $projekt["frNach"]);
+      $this->Line(237, $y, 237, $y + $h);
+      $this->Line(237, $y + $h, 287, $y + $h);
+      $this->Line(287, $y, 287, $y + $h);
+      $this->ln($h);
+    }
+
+    function Header() {
+      $this->Image("pictures/Logo_Farbe.jpg", 10, 6, 30); // pfad ,x ,y , size
+      $this->SetFont('freeserif', 'B', 15);
+      $this->Ln(11);
+      $this->Cell(30);
+      $this->Cell(66, 10, 'Projektwoche ' . date("Y"));
+      $this->ln(13);
+    }
+  }
+
+  // Anfrage verarbeiten
+  $pdf = new printPDF('L', 'mm', 'A4');
+  if (!empty($_GET["print"]) && !empty($_GET["projekt"]) && $_GET["print"] == "projekt") {
+    if ($_GET['projekt'] == "all") {
+      foreach (dbRead("../data/projekte.csv") as $projekt) {
+        $pdf->printProjekt($projekt);
+      }
+    }
+    else {
+      foreach (dbSearch("../data/projekte.csv", "id", $_GET['projekt'], true) as $projekt) {
+        $pdf->printProjekt($projekt);
+      }
+    }
+  }
+  elseif (!empty($_GET["print"]) && $_GET["print"] == "students") {
+    die("Ich bin ein Platzhalter für eine nicht implementierte Funktion!");
+  }
+  else {
+    die("Ungültige Anfrage");
+  }
+  $pdf->Output();
+?>
