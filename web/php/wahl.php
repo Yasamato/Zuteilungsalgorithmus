45a0f0db7719f9da5bc19e0c7311e6fcb9f64443
Agent77326 <agent77326@gmail.com>
2019-03-26 20:55:45 +0100

    Wahl fix
    #65

--- web/php/wahl.php
+++ web/php/wahl.php
@@ -1,24 +1,22 @@
 <?php
 if (isLogin()) {
 	if (!empty($_POST['wahl']) && count($_POST['wahl']) == 4 && $_SESSION['benutzer']['typ'] != "teachers") {
-		$projekte = dbRead("../data/projekte.csv");
 		$valid = true;
 		foreach ($_POST['wahl'] as $p) {
-			$projekt = getProjektInfo($p);
+			$projekt = getProjektInfo($projekte, $p);
 			if (empty($projekt) || $projekt['minKlasse'] > $_SESSION['benutzer']['stufe'] && $projekt['maxKlasse'] < $_SESSION['benutzer']['stufe']) {
 				$valid = false;
 			}
 		}
 
 		if ($valid) {
-
 			$wahl = [
 				$_SESSION['benutzer']['uid'],
 				$_SESSION['benutzer']['vorname'],
 				$_SESSION['benutzer']['nachname'],
 				$_SESSION['benutzer']['stufe'],
 				$_SESSION['benutzer']['klasse'],
-				implode("§", $_POST['wahl']), // warum haben wir hier §? weil ir es können :)
+				implode("§", $_POST['wahl']), // warum haben wir hier §? weil wir es können :)
 				""
 			];
 			// Überschreiben des vorigen Eintrags
